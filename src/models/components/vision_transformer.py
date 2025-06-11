15cf3d5ab7c85168c7f334921e704f0933888b86
LudovicTuncay <tuncay-ludovic@outlook.fr>
2025-06-11 23:32:45 +0200

    Changed to Flash Attention

--- src/models/components/vision_transformer.py
+++ src/models/components/vision_transformer.py
@@ -19,26 +19,24 @@ from src.models.components.positional_embedding import (
 )
 
 from src.models.components.mlp import MLP
-from src.models.components.attentions import Attention
 from src.models.components.drop_path import DropPath
 
+from flash_attn.modules.mha import MHA
 
 class Block(nn.Module):
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, use_flash_attn=True):
         super().__init__()
         self.norm1 = norm_layer(dim)
-        self.attn = Attention( # TODO Change to FlashAttention
-            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+        self.attn = MHA(embed_dim=dim, num_heads=num_heads, dropout=attn_drop, qkv_proj_bias=qkv_bias, use_flash_attn=use_flash_attn)
+        
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = MLP(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
 
-    def forward(self, x, return_attention=False):
-        y, attn = self.attn(self.norm1(x))
-        if return_attention:
-            return attn
+    def forward(self, x):
+        y = self.attn(self.norm1(x))
         x = x + self.drop_path(y)
         x = x + self.drop_path(self.mlp(self.norm2(x)))
         return x
@@ -109,6 +107,7 @@ class VisionTransformerPredictor(nn.Module):
         drop_path_rate=0.0,
         norm_layer = nn.LayerNorm,
         init_std=0.02,
+        use_flash_attn=True,
         **kwargs
     ):
         super().__init__()
@@ -128,36 +127,24 @@ class VisionTransformerPredictor(nn.Module):
         self.predictor_blocks = nn.ModuleList([
             Block(
                 dim=predictor_embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer, use_flash_attn=use_flash_attn)
             for i in range(depth)])
         self.predictor_norm = norm_layer(predictor_embed_dim)
         self.predictor_proj = nn.Linear(predictor_embed_dim, embed_dim, bias=True)
         # ------
         self.init_std = init_std
         trunc_normal_(self.mask_token, std=self.init_std)
-        self.apply(self._init_weights)
-        self.fix_init_weight()
+        self.apply(self._init_weights_vit_timm)
+        # self.fix_init_weight()
 
-    def fix_init_weight(self):
-        def rescale(param, layer_id):
-            param.div_(math.sqrt(2.0 * layer_id))
-
-        for layer_id, layer in enumerate(self.predictor_blocks):
-            rescale(layer.attn.proj.weight.data, layer_id + 1)
-            rescale(layer.mlp.fc2.weight.data, layer_id + 1)
-
-    def _init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            trunc_normal_(m.weight, std=self.init_std)
-            if isinstance(m, nn.Linear) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
-        elif isinstance(m, nn.Conv2d):
-            trunc_normal_(m.weight, std=self.init_std)
-            if m.bias is not None:
-                nn.init.constant_(m.bias, 0)
+    def _init_weights_vit_timm(module: nn.Module, name: str = ""):
+        """ViT weight initialization, original timm impl (for reproducibility)"""
+        if isinstance(module, nn.Linear):
+            trunc_normal_(module.weight, std=0.02)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        elif hasattr(module, "init_weights"):
+            module.init_weights()
 
     def forward(self, x, context_masks, target_masks):
         assert (context_masks is not None) and (target_masks is not None), 'Cannot run predictor without mask indices'
@@ -224,6 +211,7 @@ class VisionTransformer(nn.Module):
         drop_path_rate=0.0,
         norm_layer=nn.LayerNorm,
         init_std=0.02,
+        use_flash_attn=True,
         **kwargs
     ):
         super().__init__()
@@ -255,34 +243,22 @@ class VisionTransformer(nn.Module):
         self.blocks = nn.ModuleList([
             Block(
                 dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer, use_flash_attn=use_flash_attn)
             for i in range(depth)])
         self.norm = norm_layer(embed_dim)
         # ------
         self.init_std = init_std
-        self.apply(self._init_weights)
-        self.fix_init_weight()
+        self.apply(self._init_weights_vit_timm)
+        # self.fix_init_weight()
 
-    def fix_init_weight(self):
-        def rescale(param, layer_id):
-            param.div_(math.sqrt(2.0 * layer_id))
-
-        for layer_id, layer in enumerate(self.blocks):
-            rescale(layer.attn.proj.weight.data, layer_id + 1)
-            rescale(layer.mlp.fc2.weight.data, layer_id + 1)
-
-    def _init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            trunc_normal_(m.weight, std=self.init_std)
-            if isinstance(m, nn.Linear) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
-        elif isinstance(m, nn.Conv2d):
-            trunc_normal_(m.weight, std=self.init_std)
-            if m.bias is not None:
-                nn.init.constant_(m.bias, 0)
+    def _init_weights_vit_timm(module: nn.Module, name: str = ""):
+        """ViT weight initialization, original timm impl (for reproducibility)"""
+        if isinstance(module, nn.Linear):
+            trunc_normal_(module.weight, std=0.02)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        elif hasattr(module, "init_weights"):
+            module.init_weights()
 
     def forward(self, x, context_masks=None):
         # if context_masks is not None:
